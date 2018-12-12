6df27c0d9e54296ae42bb26bb9a6cc9533738578
Andrii Holovko <andriy.holovko@gmail.com>
2018-12-12 21:48:57 +0200

    prime factor of one is the empty set
    This constrains the problem, so the test becomes more specific.

--- main_test.go
+++ main_test.go
@@ -1,6 +1,27 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
 
-func TestNothing(t *testing.T) {
+func TestPrimeFactors(t *testing.T) {
+	tests := []struct {
+		arg  int
+		want []int
+	}{
+		{1, []int{}},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%d", tt.arg), func(t *testing.T) {
+			if got := primeFactors(tt.arg); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("primeFactors(%d) = %v, want %v", tt.arg, got, tt.want)
+			}
+		})
+	}
+}
+
+func primeFactors(n int) []int {
+	return []int{}
 }
