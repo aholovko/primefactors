1ccf55144f0e4767203ac75a58d03336bea95f86
Andrii Holovko <andriy.holovko@gmail.com>
2018-12-13 09:47:12 +0200

    prime factors of 4

--- main_test.go
+++ main_test.go
@@ -14,6 +14,7 @@ func TestPrimeFactors(t *testing.T) {
 		{1, []int{}},
 		{2, []int{2}},
 		{3, []int{3}},
+		{4, []int{2, 2}},
 	}
 	for _, tt := range tests {
 		t.Run(fmt.Sprintf("%d", tt.arg), func(t *testing.T) {
@@ -27,7 +28,13 @@ func TestPrimeFactors(t *testing.T) {
 func primeFactors(n int) []int {
 	factors := []int{}
 	if n > 1 {
-		factors = append(factors, n)
+		if n%2 == 0 {
+			factors = append(factors, 2)
+			n /= 2
+		}
+		if n > 1 {
+			factors = append(factors, n)
+		}
 	}
 	return factors
 }
